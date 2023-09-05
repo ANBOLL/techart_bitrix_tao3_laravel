25ffda93f9d46e7a17afce316aa6b894dcb3ab10
Andrey Boldyrev <myglavniyacount@gmail.com>
2023-09-05 15:37:14 +0300

    435224: Add background-image.

--- frontend/src/block/common/test/test.blade.php
+++ frontend/src/block/common/test/test.blade.php
@@ -2,4 +2,5 @@
 	<div class="{{ $block->elem('title') }}">Это тест!!!</div>
 	<div class="{{ $block->elem('first') }}">Первая строка.</div>
 	<div class="{{ $block->elem('second') }}">Вторая строка.</div>
+	<div class="{{ $block->elem('test-image') }}" style="background-image: url({{Storage::url('public/image/Group2.png')}});"></div>
 </div>
